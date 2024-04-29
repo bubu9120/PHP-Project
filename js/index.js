a85eb2cdf07f0d70153563a3a1b9a10755c1df6c
bubu9120 <163829799+bubu9120@users.noreply.github.com>
2024-04-29 16:35:57 +0200

    basic index

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/* Default styles for mobile */
+body {
+    
+main {
+
+
+}
+
+  }
+  
+  /* Media query for tablets and larger screens */
+  @media (min-width: 768px) {
+    body {
+     
+    }
+  }
+  
+  /* Media query for desktops and larger screens */
+  @media (min-width: 1024px) {
+    body {
+     
+    }
+  }
+  
