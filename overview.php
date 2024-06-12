facf647dd45991e9769c30c652aa5a6abf48ffa1
dariowenger <163829902+rioisalive@users.noreply.github.com>
2024-06-12 21:07:26 +0200

    overview

--- overview.php
+++ overview.php
@@ -64,6 +64,7 @@ function isChecked($sessionData, $question, $value)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="css/overview.css">
 </head>
 
 <body>
@@ -72,60 +73,123 @@ function isChecked($sessionData, $question, $value)
         <form method="post" action="">
             <h1 class="hellotext">We help you to find your <br>right device!</h1>
             <fieldset id="overview-forms">
-                <h2>Overview</h2>
-                <p>Please check your answers.</p>
-                <br>
+
+                <div class="container">
+                    <h1 class="card__title">Two steps left.</h1><br>
+                    <p>Please check your Awnsers and chlick next for the last question.</p>
+                    <div class="item">
+                        <span class="label">Question 1</span>
+                        <div class="radio-group">
+                            <input type="radio" name="qone" id="qoneno" value="no" <?= isChecked($responses, 'qone', 'no') ?> required>
+                            <label for="qoneno">No</label>
+                            <input type="radio" name="qone" id="qoneyes" value="yes" <?= isChecked($responses, 'qone', 'yes') ?> required>
+                            <label for="qoneyes">Yes</label>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="label">Question 2</span>
+                        <div class="radio-group">
+                            <input type="radio" name="qtwo" id="qtwono" value="no" <?= isChecked($responses, 'qtwo', 'no') ?> required>
+                            <label for="qtwono">No</label>
+                            <input type="radio" name="qtwo" id="qtwoyes" value="yes" <?= isChecked($responses, 'qtwo', 'yes') ?> required>
+                            <label for="qtwoyes">Yes</label>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="label">Question 3</span>
+                        <div class="radio-group">
+                            <input type="radio" name="qthree" id="qthreeno" value="no" <?= isChecked($responses, 'qthree', 'no') ?> required>
+                            <label for="qthreeno">No</label>
+                            <input type="radio" name="qthree" id="qthreeyes" value="yes" <?= isChecked($responses, 'qthree', 'yes') ?> required>
+                            <label for="qthreeyes">Yes</label>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="label">Question 4</span>
+                        <div class="radio-group">
+                            <input type="radio" name="qfour" id="qfourno" value="no" <?= isChecked($responses, 'qfour', 'no') ?> required>
+                            <label for="qfourno">No</label>
+                            <input type="radio" name="qfour" id="qfouryes" value="yes" <?= isChecked($responses, 'qfour', 'yes') ?> required>
+                            <label for="qfouryes">Yes</label>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="label">Question 5</span>
+                        <div class="radio-group">
+                            <input type="radio" name="qfive" id="qfiveno" value="no" <?= isChecked($responses, 'qfive', 'no') ?> required>
+                            <label for="qfiveno">No</label>
+                            <input type="radio" name="qfive" id="qfiveyes" value="yes" <?= isChecked($responses, 'qfive', 'yes') ?> required>
+                            <label for="qfiveyes">Yes</label>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="label">Question 6</span>
+                        <div class="radio-group">
+                            <input type="radio" name="qsix" id="qsixno" value="no" <?= isChecked($responses, 'qsix', 'no') ?> required>
+                            <label for="qsixno">No</label>
+                            <input type="radio" name="qsix" id="qsixyes" value="yes" <?= isChecked($responses, 'qsix', 'yes') ?> required>
+                            <label for="qsixyes">Yes</label>
+                        </div>
+                    </div>
+                </div>
+                <!-- <br>
                 <p>
                     <label for="qone">Are your looking for a new device for the first time?</label><br>
-                    <input type="radio" name="qone" id="qoneyes" value="yes" <?= isChecked($responses, 'qone', 'yes') ?> required>
-                    <label for="qoneyes">Yes</label>
                     <input type="radio" name="qone" id="qoneno" value="no" <?= isChecked($responses, 'qone', 'no') ?> required>
                     <label for="qoneno">No</label>
+                    <input type="radio" name="qone" id="qoneyes" value="yes" <?= isChecked($responses, 'qone', 'yes') ?> required>
+                    <label for="qoneyes">Yes</label>
+
                 </p>
                 <br>
                 <p>
                     <label for="qtwo">Creative or Administration?</label><br>
-                    <input type="radio" name="qtwo" id="qtwoyes" value="yes" <?= isChecked($responses, 'qtwo', 'yes') ?> required>
-                    <label for="qtwoyes">Yes</label>
                     <input type="radio" name="qtwo" id="qtwono" value="no" <?= isChecked($responses, 'qtwo', 'no') ?> required>
                     <label for="qtwono">No</label>
+                    <input type="radio" name="qtwo" id="qtwoyes" value="yes" <?= isChecked($responses, 'qtwo', 'yes') ?> required>
+                    <label for="qtwoyes">Yes</label>
+
                 </p>
                 <br>
                 <p>
                     <label for="qthree">Question 3</label><br>
-                    <input type="radio" name="qthree" id="qthreeyes" value="yes" <?= isChecked($responses, 'qthree', 'yes') ?> required>
-                    <label for="qthreeyes">Yes</label>
                     <input type="radio" name="qthree" id="qthreeno" value="no" <?= isChecked($responses, 'qthree', 'no') ?> required>
                     <label for="qthreeno">No</label>
+                    <input type="radio" name="qthree" id="qthreeyes" value="yes" <?= isChecked($responses, 'qthree', 'yes') ?> required>
+                    <label for="qthreeyes">Yes</label>
+
                 </p>
                 <br>
                 <p>
                     <label for="qfour">Question 4</label><br>
-                    <input type="radio" name="qfour" id="qfouryes" value="yes" <?= isChecked($responses, 'qfour', 'yes') ?> required>
-                    <label for="qfouryes">Yes</label>
                     <input type="radio" name="qfour" id="qfourno" value="no" <?= isChecked($responses, 'qfour', 'no') ?> required>
                     <label for="qfourno">No</label>
+                    <input type="radio" name="qfour" id="qfouryes" value="yes" <?= isChecked($responses, 'qfour', 'yes') ?> required>
+                    <label for="qfouryes">Yes</label>
+
                 </p>
                 <br>
                 <p>
                     <label for="qfive">Question 5</label><br>
-                    <input type="radio" name="qfive" id="qfiveyes" value="yes" <?= isChecked($responses, 'qfive', 'yes') ?> required>
-                    <label for="qfiveyes">Yes</label>
                     <input type="radio" name="qfive" id="qfiveno" value="no" <?= isChecked($responses, 'qfive', 'no') ?> required>
                     <label for="qfiveno">No</label>
+                    <input type="radio" name="qfive" id="qfiveyes" value="yes" <?= isChecked($responses, 'qfive', 'yes') ?> required>
+                    <label for="qfiveyes">Yes</label>
+
                 </p>
                 <br>
                 <p>
                     <label for="qsix">Question 6</label><br>
-                    <input type="radio" name="qsix" id="qsixyes" value="yes" <?= isChecked($responses, 'qsix', 'yes') ?> required>
-                    <label for="qsixyes">Yes</label>
                     <input type="radio" name="qsix" id="qsixno" value="no" <?= isChecked($responses, 'qsix', 'no') ?> required>
                     <label for="qsixno">No</label>
-                </p>
-                <p>
+                    <input type="radio" name="qsix" id="qsixyes" value="yes" <?= isChecked($responses, 'qsix', 'yes') ?> required>
+                    <label for="qsixyes">Yes</label>
+
+                </p> -->
+                <!-- <p>
                     <br />
                     <input name="next" type="submit" value="Show Device" />
-                </p>
+                </p> -->
             </fieldset>
         </form>
     </div>
