10e34e67ce85197741751a19a90d79d9ded07a38
dariowenger <163829902+rioisalive@users.noreply.github.com>
2024-06-15 19:45:41 +0200

    del

--- helpRequest.php
+++ helpRequest.php
@@ -69,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/Logo.svg" type="image/x-icon" />
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 
--- yourdevice.php
+++ yourdevice.php
@@ -29,7 +29,7 @@ if (isset($_POST['jumpToStart'])) {
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="img/Logo.svg" type="image/x-icon" />
-  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/stylesheet.css">
   <link rel="stylesheet" href="css/overview.css">
 </head>
